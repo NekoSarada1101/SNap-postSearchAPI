5ede9f12041b14b7086fc8a0fb255bbaa3cd164b
NekoSarada1101 <ryoyukikazu11@gmail.com>
2020-12-16 00:24:41 +0900

    feat: MySQLから検索する

---
+++ main.py
@@ -0,0 +1,86 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+
+import sqlalchemy
+from flask import Flask, request, make_response
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    response_data = {"body": "hello", "status": "200"}
+    return make_response(json.dumps(response_data))
+
+
+@app.route('/search')
+def searchPost():
+    word = request.args.get('word')
+    db = init_unix_connection_engine()
+    response_data = fetch_search_post(db, word)
+
+    return make_response(json.dumps(response_data))
+
+
+def init_unix_connection_engine():
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            database=db_name,  # e.g. "my-database-name"
+            query={
+                "unix_socket": "{}/{}".format(
+                    db_socket_dir,  # e.g. "/cloudsql"
+                    cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+            }
+        ),
+        # ... Specify additional properties here.
+    )
+    return pool
+
+
+def fetch_search_post(db, word):
+    with db.connect() as conn:
+        result = conn.execute("SELECT * FROM posts WHERE message LIKE %s", "%" + word + "%").fetchall()
+        data_list = []
+        for data in result:
+            data_list.append({"id": data[0], "message": data[1]})
+        response_data = {
+            "data": data_list,
+            "status": 200
+        }
+        return response_data
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+    # searchPost()
