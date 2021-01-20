bd9bb78fe5bf0c5ec75ca75b14b346a6a00239e1
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-01-20 14:03:48 +0900

    chore: コメント整理

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import json
 import os
 
 import sqlalchemy
 from flask import Flask, request, make_response
 
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
 app = Flask(__name__)
 
 
@@ -60,7 +44,6 @@ def init_unix_connection_engine():
                     cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
             }
         ),
-        # ... Specify additional properties here.
     )
     return pool
 
@@ -83,4 +66,3 @@ if __name__ == '__main__':
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-    # searchPost()
